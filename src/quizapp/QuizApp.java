e6fe9ade27c06f325575f122a8ffe7d573203b09
KAIRA GOTZE <issuagets@gmail.com>
2018-04-21 14:43:15 +0300

    designed

--- src/quizapp/QuizApp.java
+++ src/quizapp/QuizApp.java
@@ -4,18 +4,21 @@
  * and open the template in the editor.
  */
 package quizapp;
-
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 /**
  *
  * @author KairaMGotze
  */
 public class QuizApp {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
     }
+ 
+  
     
 }
